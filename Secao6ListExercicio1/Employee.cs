3588f1d9e5456cb23c047f32fa6f241ade2fdda5
Fernando C. Teixeira <fct0316@gmail.com>
2023-05-12 19:28:18 -0300

    Adicionar arquivos de projeto.

---
+++ Secao6ListExercicio1/Employee.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Exercicio1 {
+    class Employee {
+        public int Id { get; private set; }
+        public string Name { get; set; }
+
+        public double Salary { get; set; }
+
+        public Employee() { 
+        }
+
+        public Employee(int id, string name, double salary) {
+            this.Id = id;
+            this.Name = name; 
+            this.Salary = salary;
+        }
+
+        public void IncreaseSalary(double percentage) {
+            Salary += Salary * (percentage / 100);
+        }
+
+        public override string ToString() {
+            return Id
+                +", "
+                +Name
+                +", $"
+                +Salary.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Secao6ListExercicio1/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+
+namespace Exercicio1 {
+    /*Fazer um programa para ler um número inteiro N e depois os dados (id, nome e salario) de
+N funcionários. Não deve haver repetição de id.
+
+Em seguida, efetuar o aumento de X por cento no salário de um determinado funcionário.
+Para isso, o programa deve ler um id e o valor X. Se o id informado não existir, mostrar uma
+mensagem e abortar a operação. 
+    
+    Ao final, mostrar a listagem atualizada dos funcionários,
+conforme exemplos.
+
+Lembre-se de aplicar a técnica de encapsulamento para não permitir que o salário possa
+ser mudado livremente. Um salário só pode ser aumentado com base em uma operação de
+aumento por porcentagem dada.*/
+
+    class Program {
+
+        static void Main(string[] args) {
+
+            List <Employee> employee = new List <Employee> ();
+            int id;
+            Console.Write("How many employees will be registered? ");
+            int amount = int.Parse(Console.ReadLine());
+            for(int i = 1; i <= amount; i++) {
+                Console.WriteLine($"Employee #{i}:");
+                Console.Write("Id: ");
+                id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salary: $");
+                double salary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                employee.Add(new Employee(id, name, salary));
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter the employee id that will have salary increase : ");
+            id = int.Parse(Console.ReadLine());
+            Employee idTest = employee.Find(x => x.Id == id);
+            if (idTest == null) {
+                Console.WriteLine("This id does not exist!");
+            }
+            else {
+                Console.Write("Enter the percentage: ");
+                double percentage = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                idTest.IncreaseSalary(percentage);
+            }
+
+
+            Console.WriteLine();
+            Console.WriteLine("Updated list of employees:");
+            foreach(Employee func in employee) {
+                Console.WriteLine(func);
+            }
+
+
+        }
+    }
+}
